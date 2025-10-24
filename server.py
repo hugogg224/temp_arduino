f10695bb50ea72acc21daac150c6498b4c9cc7dc
hugogg224 <ng013418@gmail.com>
2025-10-23 20:15:24 -0600

    Refactor server.py for dynamic port and response format
    Updated the server to use an environment variable for the port and changed the response format for the '/ultimo' route.

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, jsonify
+import os
+from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)  # Permitir CORS desde cualquier origen
+CORS(app)  # permite acceder desde cualquier web
 
-# Conexión a MongoDB
+# MongoDB
 mongo_uri = "mongodb+srv://hh:BYvwcNLuZ7EYwT0j@cluster0.axpsmqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client_db = MongoClient(mongo_uri)
 db = client_db.sensor
@@ -14,7 +15,8 @@ collection = db.Temperatura
 @app.route("/ultimo")
 def ultimo():
     doc = collection.find_one(sort=[('_id', -1)])
-    return jsonify({"tempC": float(doc['tempC'])})
+    return str(doc['tempC'])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # usa el puerto que da Railway
+    app.run(host="0.0.0.0", port=port)
